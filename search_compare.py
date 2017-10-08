d7698450ced6601edd2ad4ced1e4d96bde1ad01a
Rommel Lasso <soccerply@msn.com>
2017-10-08 03:53:52 -0400

    Search Complete

--- search_compare.py
+++ search_compare.py
@@ -36,11 +36,44 @@ def sequential_search(list, value):
     #print(duration)
     return duration, found
 
-def ordered_sequential_search():
-    print 'os'
+def ordered_sequential_search(list, value):
+    pos = 0
+    found = False
 
-def binary_search_iterative():
-    print 'bs'
+    start = time.time()
+    while pos < len(list) and not found:
+        if list[pos] == value:
+            found = True
+        else:
+            pos = pos + 1
+    stop = time.time()
+    duration = stop - start
+    # print(duration)
+    return duration, found
+
+def binary_search_iterative(list, value):
+    first = 0
+    last = len(list) - 1
+    found = False
+
+    start = time.time()
+    print last
+
+    while first <= last and not found:
+        midpoint = (first + last) / 2
+
+        if list[midpoint] == value:
+            found = True
+        else:
+            if value < list[midpoint]:
+                last = midpoint - 1
+            else:
+                first = midpoint + 1
+
+    stop = time.time()
+    print stop
+    duration = stop - start
+    return duration, found
 
 def binary_search_recursive():
     print 'bsr'
@@ -58,7 +91,7 @@ def run_sequential_search():
         # print x, duration, value
 
     average_500 = total_duration_500 / x
-    print 'Average of 500', average_500
+    #print 'Average of 500', average_500
 
     generated_list = generate_lists(1000)
 
@@ -73,7 +106,7 @@ def run_sequential_search():
         # print x, duration, value
 
     average_1000 = total_duration_1000 / x
-    print 'Average of 1,000', average_1000
+    #print 'Average of 1,000', average_1000
 
     generated_list = generate_lists(10000)
 
@@ -88,16 +121,126 @@ def run_sequential_search():
         # print x, duration, value
 
     average_10000 = total_duration_10000 / x
-    print 'Average of 10,000', average_10000
+    #print 'Average of 10,000', average_10000
 
     final_avg_sequential = decimal.Decimal(average_500 + average_1000 + average_10000)
 
     return final_avg_sequential
 
+def run_orsequential_search():
+    #print 'hi'
+    generated_list = generate_lists(500)
+
+    total_duration_500 = 0
+    # pprint (generated_list)
+    for x in range(1, 101):
+        generated_list[x].sort()
+        #print(generated_list[x])
+        duration, value = ordered_sequential_search(generated_list[x], -1)
+
+        total_duration_500 = total_duration_500 + duration
+        #print x, duration, value
+
+    average_500 = total_duration_500 / x
+    #print 'Average of 500', average_500
+
+    generated_list = generate_lists(1000)
+
+    total_duration_1000 = 0
+    # pprint (generated_list)
+    for x in range(1, 101):
+        # print(generated_list[x])
+        generated_list[x].sort()
+        duration, value = ordered_sequential_search(generated_list[x], -1)
+        # print duration
+
+        total_duration_1000 = total_duration_1000 + duration
+        # print x, duration, value
+
+    average_1000 = total_duration_1000 / x
+    #print 'Average of 1,000', average_1000
+
+    generated_list = generate_lists(10000)
+
+    total_duration_10000 = 0
+    # pprint (generated_list)
+    for x in range(1, 101):
+        # print(generated_list[x])
+        generated_list[x].sort()
+        duration, value = ordered_sequential_search(generated_list[x], -1)
+        # print duration
+
+        total_duration_10000 = total_duration_10000 + duration
+        # print x, duration, value
+
+    average_10000 = total_duration_10000 / x
+    #print 'Average of 10,000', average_10000
+
+    final_avg_sequential = decimal.Decimal(average_500 + average_1000 + average_10000)
+
+    return final_avg_sequential
+
+def run_bsi_search():
+    #print ' hi'
+    generated_list = generate_lists(500)
+
+    total_duration_500 = 0
+    #pprint (generated_list)
+    for x in range(1, 101):
+        # print(generated_list[x])
+        duration, value = binary_search_iterative(generated_list[x], -1)
+
+        #total_duration_500 = total_duration_500 + duration
+        print x, duration, value
+
+    average_500 = total_duration_500 / x
+    print 'Average of 500', average_500
+
+    '''
+    generated_list = generate_lists(1000)
+
+    total_duration_1000 = 0
+    # pprint (generated_list)
+    for x in range(1, 101):
+        # print(generated_list[x])
+        duration, value = binary_search_iterative(generated_list[x], -1)
+
+        # total_duration_500 = total_duration_500 + duration
+        # print x, duration, value
+
+    average_1000 = total_duration_1000 / x
+    print 'Average of 1000', average_1000
+
+    generated_list = generate_lists(10000)
+
+    total_duration_10000 = 0
+    # pprint (generated_list)
+    for x in range(1, 101):
+        # print(generated_list[x])
+        duration, value = binary_search_iterative(generated_list[x], -1)
+
+        # total_duration_500 = total_duration_500 + duration
+        # print x, duration, value
+
+    average_10000 = total_duration_10000 / x
+    print 'Average of 1,000', average_10000
+    '''
+
+
+
 def main():
 
-    calc_seq = run_sequential_search()
-    print 'Sequential Search took {} seconds to run, on average'.format(round(calc_seq,5))
+    #calc_seq = run_sequential_search()
+    #print 'Sequential Search took {} seconds to run, on average'.format(round(calc_seq,5))
+
+    #calc_seq = run_orsequential_search()
+    #print 'Ordered Sequential Search took {} seconds to run, on average'.format(round(calc_seq, 5))
+
+    calc_seq = run_bsi_search()
+    #print 'Recursive Binary Search took {} seconds to run, on average'.format(round(calc_seq, 5))
+
+    #calc_seq = run_bsr_search()
+    #print 'Sequential Search took {} seconds to run, on average'.format(round(calc_seq, 5))
 
 
 
